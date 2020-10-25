b4fd9ca29ecfb5aa82982115e7ec911f6fd01f36
Nathalie <natg.inf@gmail.com>
2020-10-25 21:59:52 +0100

    Update settings file

--- books_online/settings.py
+++ books_online/settings.py
@@ -2,3 +2,7 @@
 # coding: utf-8
 
 SCRAPE_URL = 'http://books.toscrape.com/'
+FIELDNAMES = ['product_page_url', 'universal_product_code', 'title',
+              'price_including_tax', 'price_excluding_tax',
+              'quantity_available', 'product_description', 'category',
+              'review_rating', 'image_url']
