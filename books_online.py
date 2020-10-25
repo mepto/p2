4872541381b2c07f380ff01ab29c567f0f6dccdf
Nathalie <natg.inf@gmail.com>
2020-10-25 11:05:15 +0100

    Update programme launch file

--- books_online.py
+++ books_online.py
@@ -2,14 +2,13 @@
 # coding: utf-8
 
 """
-:synopsis: Start scraping program from here
+:synopsis: Start scraping programme from here
 """
 
 from books_online import main as scrape
 
 
 # Launch main loop from project root
-# scrape.main()
 
 if __name__ == '__main__':
     scrape.main()
