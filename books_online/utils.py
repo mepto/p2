6f0a1e3440e0bf54140d2af56bac2d479369f3fa
Nathalie <natg.inf@gmail.com>
2020-10-25 21:49:35 +0100

    Add get_folder def

--- books_online/utils.py
+++ books_online/utils.py
@@ -1,5 +1,6 @@
 #! /usr/bin/python
 # coding: utf-8
+import os
 
 
 def get_user_choice(message):
@@ -11,3 +12,22 @@ def get_user_choice(message):
     while not user_choice.isdigit():
         user_choice = input(f'Incorrect entry "{user_choice}". {message}')
     return user_choice
+
+
+def get_folder(folder_path):
+    """
+    Check if assets folder exists and creates it if necessary
+    :return: folder path in assets folder
+    """
+    current_folder = os.path.dirname(os.path.realpath(__file__))
+    current_folder = current_folder.replace('\\', '/')
+
+    assets_path = f'{current_folder}/assets'
+    if not os.path.exists(assets_path):
+        os.mkdir(assets_path)
+
+    folder_path = f'{assets_path}/{folder_path}'
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+
+    return folder_path
