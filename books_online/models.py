d2522c3f2409cc1cf507e186baa8cc910770b1aa
Nathalie <natg.inf@gmail.com>
2020-10-25 11:02:39 +0100

    Update Category class

--- books_online/models.py
+++ books_online/models.py
@@ -1,6 +1,6 @@
 #! /usr/bin/python
 # coding: utf-8
-
+from math import trunc
 from urllib.parse import urljoin
 import requests
 
@@ -54,11 +54,30 @@ class Category:
     """
     total_pages = 1
 
-    def __init__(self, url):
+    def __init__(self, url=''):
         self.category_products = []
+        products_per_page = 20
+
         page = Page(url).scrape()
+
         if page:
-            ...
+            self.get_page_products(url)
+            total_results = int(
+                page.select('form.form-horizontal strong')[0].text)
+            if total_results > products_per_page:
+                extra_pages = trunc(total_results / products_per_page)
+                base_url = url[:url.rfind('/')]
+                for result_page in range(2, extra_pages + 2):
+                    extra_page_url = f'{base_url}/page-{result_page}.html'
+                    self.get_page_products(extra_page_url)
+
+    def get_page_products(self, url):
+        current_page = Page(url).scrape()
+        links = current_page.select('section ol li article.product_pod h3 a')
+        for link in links:
+            stripped_link = link.get('href').strip('../')
+            self.category_products.append(SCRAPE_URL + 'catalogue/' +
+                                          stripped_link)
 
     def data(self):
         return self.category_products
