11a393fb04b8494f290275eaf8743c682100a731
Nathalie <natg.inf@gmail.com>
2020-10-29 10:40:12 +0100

    Process to change logic from main loop to objects

--- books_online/main.py
+++ books_online/main.py
@@ -19,22 +19,38 @@ def main():
     images_download = get_user_choice('Would you like to download the books '
                                       'images files? (1) Yes, (0) No', [0, 1])
     print(PATIENCE_MESSAGE)
-    categories = AllCategories(scrape_type).data()
+    # print(AllCategories(scrape_type).get_books_list())
+    # categories = AllCategories(scrape_type).data()
+    categories = AllCategories(scrape_type).get_books_list()
     for category in categories:
         exports_folder = get_folder('exports')
-        category_name = category['category']
-        filename = f"{exports_folder}/{category_name}.csv"
-
+        filename = f"{exports_folder}/{category}.csv"
         with open(filename, 'w', encoding="utf-8-sig", newline='') as csvfile:
-            print(f"Creating file {category_name}.csv")
+            print(f"Creating file {category}.csv")
             csv_writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES,
                                         delimiter='|')
             csv_writer.writeheader()
-            category_products = Category(category['href']).data()
-            for product in category_products:
-                book = Book(product)
-                csv_writer.writerow(book.data())
+            # category_products = Category(category['href']).data()
+            for book in category:
+                # book = Book(product)
+                csv_writer.writerow(book)
                 if images_download == 1:
                     get_image(book)
+    # for category in categories:
+    #     exports_folder = get_folder('exports')
+    #     category_name = category['category']
+    #     filename = f"{exports_folder}/{category_name}.csv"
+    #
+    #     with open(filename, 'w', encoding="utf-8-sig", newline='') as csvfile:
+    #         print(f"Creating file {category_name}.csv")
+    #         csv_writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES,
+    #                                     delimiter='|')
+    #         csv_writer.writeheader()
+    #         category_products = Category(category['href']).data()
+    #         for product in category_products:
+    #             book = Book(product)
+    #             csv_writer.writerow(book.data())
+    #             if images_download == 1:
+    #                 get_image(book)
 
     print('Export process is done.')
